77f26e7360898025a2ca68ca57c15117dff831d5
Ashley <crashly0302@gmail.com>
2023-04-25 17:54:42 -0600

    "Fourth Push: Moved Recipe to its own .java file in the folder"

--- src/main/java/ProjectFolder/Test.java
+++ src/main/java/ProjectFolder/Test.java
@@ -330,33 +330,4 @@ public class Test extends Application {
         }
         return null;
     }
-
-    private static class Recipe {
-        private String name;
-        private String ingredients;
-        private String instructions;
-        public Recipe(String name, String ingredients, String instructions) {
-            this.name = name;
-            this.ingredients = ingredients;
-            this.instructions = instructions;
-        }
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-        public String getIngredients() {
-            return ingredients;
-        }
-        public void setIngredients(String ingredients) {
-            this.ingredients = ingredients;
-        }
-        public String getInstructions() {
-            return instructions;
-        }
-        public void setInstructions(String instructions) {
-            this.instructions = instructions;
-        }
-    }
 }
